cf1ff8455c34f7009c38be9c3c7ef68ba9a7dd79
KRISHNAMANGAI <124129483+KRISHNAMANGAI@users.noreply.github.com>
2023-02-01 13:35:24 +0530

    Update streamlit_app.py
    Added two more item under breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,6 @@ streamlit.title("My Parents New Healthy DIner")
 streamlit.header("Breakfast Menu")
 streamlit.text("Bluberry Oatmeal")
 streamlit.text("French Toast")
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
                
