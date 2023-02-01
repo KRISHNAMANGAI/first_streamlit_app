15e58869e3dc8201e8d980496be89952368c29cb
KRISHNAMANGAI <124129483+KRISHNAMANGAI@users.noreply.github.com>
2023-02-01 13:16:24 +0530

    Update streamlit_app.py
    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title("My Parents New Healthy DIner")
 streamlit.header("Breakfast Menu")
-streamlit.text("Bluberry Oatmeal)
+streamlit.text("Bluberry Oatmeal")
 streamlit.text("French Toast")
                
