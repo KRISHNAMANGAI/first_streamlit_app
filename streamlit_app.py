6ae2ba1392fc1d6df40f03c610ef97849ddf4504
KRISHNAMANGAI <124129483+KRISHNAMANGAI@users.noreply.github.com>
2023-02-01 21:54:23 +0530

    Update streamlit_app.py
    Added streamlit package library code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,10 @@
+import streamlit
+streamlit.title("My Mom's New Healthy DIner")
+streamlit.header("Breakfast Favorites")
+streamlit.text("🥣 Bluberry Oatmeal")
+streamlit.text("🍞French Toast")
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie’)
+streamlit.text('🐔Hard-Boiled Free-Range Egg’)
+streamlit.text('🍞Avocado Toast’)
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
