6494b3d9bb441faf4c2e20ffeeea5df48eefd12f
KRISHNAMANGAI <124129483+KRISHNAMANGAI@users.noreply.github.com>
2023-02-02 13:46:43 +0530

    Update streamlit_app.py
    Added streamlit code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
+import streamlit
+streamlit.title("My Mom's New Healthy DIner")
+streamlit.header("Breakfast Favorites")
+streamlit.text("🥣 Bluberry Oatmeal")
+streamlit.text("🍞French Toast")
+streamlit.text("🥗Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔Hard-Boiled Free-Range Egg")
+streamlit.text("🍞Avocado Toast")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 
